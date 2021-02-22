c6c63b5ed05c34792b7239446f6470551659376f
FreddyFox1 <svchaykun@mail.ru>
2021-02-22 21:47:43 +0300

    Add no flash class

---
+++ CoreEngine/NoFlash.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace CoreEngine
+{
+    /// <summary>
+    /// Отключаем эффект от ослепляющей гранаты
+    /// </summary>
+    public class NoFlash
+    {
+        public static void OnNoFlash(BaseVars baseVars)
+        {
+            float Antivirus = 0;
+            while (true)
+            {
+                int? PModel = baseVars.VAM.ReadInt32((IntPtr)baseVars.GameClient + signatures.dwLocalPlayer);
+                if (PModel != null)
+                {
+                    var PModelValueF = PModel + Netvars.m_flFlashMaxAlpha;
+                    if (PModelValueF != null)
+                    {
+                        baseVars.VAM.WriteFloat((IntPtr)PModelValueF, Antivirus);
+                    }
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}

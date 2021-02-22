3b1a4296bcff519df8c91e111aeff4eee3ed8fe6
FreddyFox1 <svchaykun@mail.ru>
2021-02-22 21:06:13 +0300

    Add winapi class

--- CoreEngine/WinApi.cs
+++ CoreEngine/WinApi.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace CoreEngine
 {
-    class WinApi
+    public class WinApi
     {
+        //Проверка нажатие клавиши
+        [DllImport("user32", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        ////Для Overlay
+        //[DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        //public static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        //[DllImport("user32.dll")]
+        //public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //public static extern int GetWindowLong(IntPtr window, int index);
+        //public static IntPtr handle = FindWindowByCaption(IntPtr.Zero, BaseVar.gameProcess);
+
+        //[DllImport("user32.dll")]
+        //public static extern IntPtr GetForegroundWindow();
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //public static extern bool GetWindowRect(IntPtr hwnd, out str.RECT lpRect);
+
+        //События мыши
+        [DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto, CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        //Нажатие на левую кнопку мыши
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //Поднятие левой кнопки мыши
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
     }
 }

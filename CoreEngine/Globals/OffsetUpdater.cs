975f266cd24792e54853f62bdf4b24c8cb533d23
FreddyFox1 <svchaykun@mail.ru>
2021-03-28 13:57:12 +0300

    Chage struct project

---
+++ CoreEngine/Globals/OffsetUpdater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEngine
+{
+    /// <summary>
+    /// Автоматическое обновление оффсетов с Hazedumper
+    /// Automatical update offsets from Hazedumper
+    /// </summary>
+    public class OffsetUpdater : IUpdater
+    {
+        public static string WebData;
+        public OffsetUpdater()
+        {
+            DownloadOffsets();
+        }
+
+        public void DownloadOffsets()
+        {
+            WebClient web = new WebClient();
+            WebData = web.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.cs");
+        }
+
+        public void ParseLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOffset(string offsetName, ref int varName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

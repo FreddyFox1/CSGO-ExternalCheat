411bb4dd4e6038cec288fd508073133c2589cfbe
FreddyFox1 <svchaykun@mail.ru>
2021-03-28 13:56:48 +0300

    Add Abstraction for AutoUpdateOffsets

---
+++ CoreEngine/Abstractions/IUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreEngine
+{
+    /// <summary>
+    /// Automatical updater for offsets
+    /// </summary>
+    interface IUpdater
+    {
+        /// <summary>
+        /// Download string with new offesets from github
+        /// </summary>
+        void DownloadOffsets();
+
+        /// <summary>
+        /// Parsing WebData
+        /// </summary>
+        void ParseLine();
+
+        /// <summary>
+        /// Update offsets on refs
+        /// </summary>
+        /// <param name="offsetName">offset name in Dictionary</param>
+        /// <param name="varName">offset ref in Dictionary</param>
+        void UpdateOffset(string offsetName, ref int varName);
+    }
+}
